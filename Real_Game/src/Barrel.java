acbbc5b9433f17b4514881e2acc05d00856b2e4b
Harry <harry.wallbank@gmail.com>
2017-09-27 10:52:22 -0700

    commitment 2 be ignored, and an empty message aborts the commit.

--- Real_Game/src/Barrel.java
+++ Real_Game/src/Barrel.java
@@ -7,4 +7,14 @@ public class Barrel extends GameObject
 	{
 		super("Barrel",30,20,"Barrel.png");
 	}
+	@Override public void update(float dt)
+	{
+		setPositionY(getPositionY()-1);
+		if(getPositionY() == -540)
+		{
+			kill();
+			System.out.println("object is at end");
+			WaterWorld.b--;
+		}
+	}
 }
